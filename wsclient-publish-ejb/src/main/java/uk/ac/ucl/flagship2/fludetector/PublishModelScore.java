13403e3942cd7e59f0b1ae0242e5e838b8a15f85
David Guzman <d.guzman@ucl.ac.uk>
2018-05-25 16:28:38 +0100

    Scaffold for FormatModelScore bean to relocate text processing code out of PublishModelScore

--- wsclient-publish-ejb/src/main/java/uk/ac/ucl/flagship2/fludetector/PublishModelScore.java
+++ wsclient-publish-ejb/src/main/java/uk/ac/ucl/flagship2/fludetector/PublishModelScore.java
@@ -1,11 +1,16 @@
 package uk.ac.ucl.flagship2.fludetector;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.text.MessageFormat;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.inject.Inject;
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -42,10 +47,26 @@ public class PublishModelScore {
           getFromSystemOrEnv("TWITTER_TOKEN_SECRET")
   );
 
+  @Inject
+  private FormatModelScore formatter;
+
   private final MessageFormat tweetFormat = new MessageFormat(
-          "Based on Google searches, the estimated flu (Influenza-like illness) rate for England is {0} cases per 100,000 people #health #AI");
+          "Based on Google searches, the estimated flu (Influenza-like illness) rate for England on {0} is {1} cases per 100,000 people #health #AI");
 
   public void publishScore(String score) {
+
+    Properties properties = readProperties(score).orElseThrow(() -> new EJBException("Cannot read properties from message"));
+
+    String date = properties.getProperty("date", "");
+    String value = properties.getProperty("value", "");
+
+    if (date.isEmpty() || !date.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
+      throw new EJBException("Date field was not provided or date not in ISO format");
+    }
+
+    if (value.isEmpty() || !value.matches("\\d+(\\.\\d+)?")) {
+      throw new EJBException("Score value was not provided or score value is not a number");
+    }
 
     ClientConfig clientConfig = new ClientConfig();
     clientConfig.property(LoggingFeature.LOGGING_FEATURE_VERBOSITY_CLIENT, LoggingFeature.Verbosity.PAYLOAD_ANY);
@@ -60,7 +81,7 @@ public class PublishModelScore {
             .register(filterFeature)
             .build();
 
-    Object[] toFormat = {score};
+    Object[] toFormat = {date, value};
     String tweet = tweetFormat.format(toFormat);
 
     Form form = new Form();
@@ -99,6 +120,17 @@ public class PublishModelScore {
       value = System.getenv(key);
     }
     return (value != null) ? value : "";
+  }
+
+  private Optional<Properties> readProperties(String propstr) {
+    final Properties p = new Properties();
+    try {
+      p.load(new StringReader(propstr));
+    } catch (IOException ex) {
+      Logger.getLogger(PublishModelScore.class.getName()).log(Level.SEVERE, null, ex);
+      return Optional.empty();
+    }
+    return Optional.of(p);
   }
 
 }
