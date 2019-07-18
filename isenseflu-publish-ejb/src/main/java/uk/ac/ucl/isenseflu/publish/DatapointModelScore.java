ce4f34eb9de6d06c2201ea9aac2d4b705e115b1b
David Guzman <d.guzman@ucl.ac.uk>
2019-07-18 20:49:40 +0100

    Add javadoc comments to DatapointModelScore

--- isenseflu-publish-ejb/src/main/java/uk/ac/ucl/isenseflu/publish/DatapointModelScore.java
+++ isenseflu-publish-ejb/src/main/java/uk/ac/ucl/isenseflu/publish/DatapointModelScore.java
@@ -27,27 +27,54 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 /**
- *
+ * Represents the coordinates of a point in the time series of flu scores.
  * @author David Guzman
  */
 public class DatapointModelScore {
 
-  private final LocalDate date;
+  /**
+   * The date of the score.
+   */
+  private final LocalDate scoreDate;
+
+  /**
+   * The value of the score.
+   */
   private final Double scoreValue;
 
-  public DatapointModelScore(LocalDate date, Double scoreValue) {
-    this.date = date;
-    this.scoreValue = scoreValue;
+  /**
+   * Creates a data point in the time series.
+   * @param date The date of the score.
+   * @param value The value of the score.
+   */
+  public DatapointModelScore(final LocalDate date, final Double value) {
+    this.scoreDate = date;
+    this.scoreValue = value;
   }
 
+  /**
+   * Converts and formats the date of the score to String before returning it.
+   * It uses a medium text style (for example 10 Jan, 2019)
+   * @return The date of the score as a formatted String.
+   */
   public String getDate() {
-    return date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+      .ofLocalizedDate(FormatStyle.MEDIUM);
+    return scoreDate.format(dateTimeFormatter);
   }
 
+  /**
+   * Returns the date of the score for this data point with no transformation.
+   * @return The date of the score as a LocalDate.
+   */
   public LocalDate getLocalDate() {
-    return date;
+    return scoreDate;
   }
 
+  /**
+   * Returns the value of the score for this data point.
+   * @return The value of the score.
+   */
   public Double getScoreValue() {
     return scoreValue;
   }
