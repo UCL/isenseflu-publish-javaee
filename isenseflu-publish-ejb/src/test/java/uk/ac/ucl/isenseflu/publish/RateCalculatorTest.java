bd0348eff06f1a306fbaf76b2fc1a5c9ef99fb8a
David Guzman <d.guzman@ucl.ac.uk>
2019-07-18 17:39:37 +0100

    Add license header to source code

--- isenseflu-publish-ejb/src/test/java/uk/ac/ucl/isenseflu/publish/RateCalculatorTest.java
+++ isenseflu-publish-ejb/src/test/java/uk/ac/ucl/isenseflu/publish/RateCalculatorTest.java
@@ -1,3 +1,24 @@
+/*
+ * i-sense flu publish: Module of the i-sense flu application used in the publication of model scores on social media
+ *
+ * Copyright (c) 2019, UCL <https://www.ucl.ac.uk/>
+ *
+ * This file is part of i-sense flu publish
+ *
+ * i-sense flu publish is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * i-sense flu publish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with i-sense flu publish.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.ucl.isenseflu.publish;
 
 import java.util.Arrays;
